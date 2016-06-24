17e57c585201deeb67a4a47fa46913560cde50bd
Alex Liu <alex.liu@oocl.com>
2016-06-24 16:25:28 +0800

    angularjs
    Add angularJS demo

--- target/AngularSpringmvcMybatis/resources/js/app.js
+++ target/AngularSpringmvcMybatis/resources/js/app.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var AngularSpringmvcMybatis = {};
 
-var App = angular.module('AngularSpringmvcMybatis', ['AngularSpringmvcMybatis.filters', 'AngularSpringmvcMybatis.services', 'AngularSpringmvcMybatis.directives']);
-
-// Declare app level module which depends on filters, and services
-App.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/users', {
-        templateUrl: 'users/layout',
-        controller: UserController
-    });
-
-    $routeProvider.otherwise({redirectTo: '/users'});
-}]);
+angular.module('app', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ngStorage',
+    'ui.router',
+    'ui.bootstrap',
+    'ui.load',
+    'ui.jq',
+    'ui.validate',
+    'oc.lazyLoad',
+    'pascalprecht.translate'
+]);
